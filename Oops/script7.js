2180f19537bf2b1e6a0d12322642c3b34fe1fcae
Minskoleopc <minskoleopc@gmail.com>
2023-05-03 20:59:29 +0530

    added

---
+++ Oops/script7.js
@@ -0,0 +1,51 @@
+class GrandFather {
+    constructor(fn,ln){
+        this.firstName = fn
+        this.lastName = ln
+    }
+
+    displayGName(){
+        console.log(this.firstName+ this.lastName)
+    }
+
+}
+
+class Father extends GrandFather {
+   
+    constructor(fn,ln,ffn){
+       super(fn,ln)
+       this.fname = ffn
+    }
+
+    displayFName(){
+        console.log(this.fname + this.lastName)
+    }
+
+}
+
+class Son extends Father {
+
+    constructor(fn,ln,ffn,ssn){
+        super(fn,ln,ffn)
+        this.sname = ssn
+     }
+
+     displaySName(){
+        console.log(this.sname + this.lastName)
+    }
+
+
+}
+
+let chinmay = new Son("manohar","deshpande","shirish","chinmay")
+
+console.log(chinmay.firstName)
+console.log(chinmay.lastName)
+console.log(chinmay.fname)
+console.log(chinmay.sname)
+
+chinmay.displayFName()
+chinmay.displayGName()
+chinmay.displaySName()
+
+
---
+++ Oops/script8.js
@@ -0,0 +1,40 @@
+class Father {
+    constructor(fn,ln){
+        this.firstName = fn
+        this.lastName = ln
+    }
+
+    displayLastName(){
+        return this.lastName
+    }
+}
+
+class Son extends Father {
+    constructor(fn,ln,sn){
+        super(fn,ln)
+        this.sname = sn
+    }
+
+    displaySonName(){
+        console.log(this.sname + this.displayLastName())
+    }
+}
+
+class Daughter extends Father{
+    constructor(fn,ln,dn){
+        super(fn,ln)
+        this.dname = dn
+    }
+
+    displayDName(){
+        console.log(this.dname + this.displayLastName())
+    }
+
+}
+
+let chinmay = new Son("shirish","deshpande","chinmay")
+let gauri = new Daughter("shrish","deshpande","gauri")
+
+chinmay.displaySonName()
+//chinmay.displayLastName()
+gauri.displayDName()
